7919e6442a1c8de529617a704022e4e0ffbdf4ec
AndrzejDolinski <77891304+AndrzejDolinski@users.noreply.github.com>
2021-03-27 19:11:14 +0100

    Delete Zadanie1TestRunner.java

--- src/test/java/zdtestpol51bdd/zadanie1/Zadanie1TestRunner.java
+++
@@ -1,8 +0,0 @@
-package zdtestpol51bdd.zadanie1;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "/home/andrzej/Dokumenty/Kurs testera/Projekty Maven/zdtestpol51bdd/src/test/resources/Zdtestpol51bdd.zadanie1")
-public class Zadanie1TestRunner {
-}
